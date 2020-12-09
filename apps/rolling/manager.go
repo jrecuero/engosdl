0fb1ee7f3d6c1c2a447fd57d41f5056306f7df11
Jose Carlos Recuero <jose.recuero@gmail.com>
2020-12-08 18:33:22 -0800

    Create rolling game.

---
+++ apps/rolling/manager.go
@@ -0,0 +1,36 @@
+package main
+
+import "github.com/jrecuero/engosdl"
+
+// GameManager is the application game manager.
+type GameManager struct {
+	*engosdl.GameManager
+}
+
+var _ engosdl.IGameManager = (*GameManager)(nil)
+
+// NewGameManager created a new game manager instance.
+func NewGameManager(name string) *GameManager {
+	engosdl.Logger.Trace().Str("game-manager", name).Msg("new game-manager")
+	return &GameManager{
+		GameManager: engosdl.NewGameManager(name),
+	}
+}
+
+// CreateAssets creates all application assets and resources. it is called
+// before game engine starts in order to create all required assets and
+// resources.
+func (h *GameManager) CreateAssets() {
+}
+
+// DoFrameEnd is called at the end of every engine frame.
+func (h *GameManager) DoFrameEnd() {
+}
+
+// DoFrameStart is called at the start of the game frame.
+func (h *GameManager) DoFrameStart() {
+}
+
+// DoInit initializes internal game manager resources.
+func (h *GameManager) DoInit() {
+}
